a8eef4074d541fe5c2ed291994edeb936a95875e
Alexandre Pereira <alexandre.carneiro.pereira@gmail.com>
2023-10-03 22:43:42 +0100

    fix: js filterining of songs

--- app/page.tsx
+++ app/page.tsx
@@ -2,7 +2,7 @@ import { getServerSession } from 'next-auth';
 
 import { SpotifyPlayer, SpotifyWrapper } from '@/components/player';
 import { db } from '@/lib/drizzle';
-import { eq, not } from 'drizzle-orm';
+import { eq } from 'drizzle-orm';
 import { songs, likes } from '@/db/schema';
 
 export default async function Home() {
@@ -19,11 +19,37 @@ export default async function Home() {
         );
     }
 
-    const songsWithoutLikes = await db
-        .select({ id: songs.id, uri: songs.spotifyId })
+    const rows = await db
+        .select({
+            id: songs.id,
+            uri: songs.spotifyId,
+            like: likes.accoutName
+        })
         .from(songs)
-        .leftJoin(likes, eq(likes.songId, songs.id))
-        .where(not(eq(likes.accoutName, session?.user?.name as string)));
+        .leftJoin(likes, eq(likes.songId, songs.id));
+
+    const songsHashMap = rows.reduce<Record<number, { id: number; uri: string; likes: string[] }>>(
+        (acc, row) => {
+            if (!acc[row.id]) {
+                acc[row.id] = {
+                    id: row.id,
+                    uri: row.uri,
+                    likes: []
+                };
+            }
+
+            if (row.like) {
+                acc[row.id].likes.push(row.like);
+            }
+
+            return acc;
+        },
+        {}
+    );
+
+    const songsWithoutLikes = Object.values(songsHashMap).filter(
+        (song) => song.likes.length === 0 || !song.likes.includes(session.user?.name as string)
+    );
 
     songsWithoutLikes.sort(() => Math.random() - 0.5);
 
