f5be2311c5af005b06a198d4cb1c0bafa47a4d49
Alexandre Pereira <alexandre.carneiro.pereira@gmail.com>
2025-04-06 03:34:43 +0100

    fix: hmr issue in dev mode

--- app/root.tsx
+++ app/root.tsx
@@ -31,8 +31,8 @@ export function Layout({ children }: { children: React.ReactNode }) {
       <head>
         <meta charSet="utf-8" />
         <meta name="viewport" content="width=device-width, initial-scale=1" />
-        <Meta />
         <Links />
+        <Meta />
       </head>
       <body className="flex flex-col items-center gap-10">
         {data && (
